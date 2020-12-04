a82a30ca76044170da7800ae8ef1a40bd2ad791e
xiweix <xwxuan@ucdavis.edu>
2020-12-03 16:03:45 -0800

    print params

--- mnist_test.py
+++ mnist_test.py
@@ -1,5 +1,6 @@
 import os
 import time
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -55,7 +56,7 @@ class Model(nn.Module):
         x = self.layers(x)
         return x
 
-def backend(n_layers_1, output_sizes_1, drop_out_rate_1, init_lr_1, n_layers_2, output_sizes_2, drop_out_rate_2, init_lr_2, train_batch_size):
+def backend(n_layers_1, output_sizes_1, drop_out_rate_1, init_lr_1, n_layers_2, output_sizes_2, drop_out_rate_2, init_lr_2, epochs, train_batch_size, lr_step_gamma):
     assert n_layers_1 == len(output_sizes_1), f'n_layers_1 ({n_layers_1}) is not equal to len(output_sizes_1) ({len(output_sizes_1)})'
     assert n_layers_2 == len(output_sizes_2), f'n_layers_2 ({n_layers_2}) is not equal to len(output_sizes_2) ({len(output_sizes_2)})'
     use_cuda = torch.cuda.is_available()
@@ -64,19 +65,25 @@ def backend(n_layers_1, output_sizes_1, drop_out_rate_1, init_lr_1, n_layers_2, 
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
+    train_set = datasets.MNIST('../data', train=True, download=True,
                        transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
+    test_set = datasets.MNIST('../data', train=False,
                        transform=transform)
+    train_set_np = train_set.data.numpy()
+    train_set_target_np = train_set.targets.numpy()
+    test_set_np = test_set.data.numpy()
+    test_set_target_np = test_set.targets.numpy()
+    print(type(train_set_np), train_set_np.shape, train_set_target_np.shape)
+    
     train_loader = torch.utils.data.DataLoader(
-        dataset=dataset1,
+        dataset=train_set,
         batch_size=train_batch_size,
         shuffle=True,
         drop_last=True,
         pin_memory=True,
     )
     test_loader = torch.utils.data.DataLoader(
-        dataset=dataset2,
+        dataset=test_set,
         batch_size=1000,
         shuffle=False,
         drop_last=False,
@@ -86,8 +93,12 @@ def backend(n_layers_1, output_sizes_1, drop_out_rate_1, init_lr_1, n_layers_2, 
     model2 = Model(n_layers=n_layers_2, output_sizes=output_sizes_2, drop_out_rate=drop_out_rate_2).to(device)
     optimizer1 = optim.Adadelta(model1.parameters(), lr=init_lr_1)
     optimizer2 = optim.Adadelta(model2.parameters(), lr=init_lr_2)
+    scheduler1 = StepLR(optimizer1, step_size=1, gamma=lr_step_gamma)
+    scheduler2 = StepLR(optimizer2, step_size=1, gamma=lr_step_gamma)
     print(model1)
     print(model2)
+    # train and save two models
+    # for epoch in range(1, epochs + 1):
 
 
-backend(n_layers_1=2, output_sizes_1=[16, 32], drop_out_rate_1=0.2, init_lr_1=0.01, n_layers_2=3, output_sizes_2=[32, 64, 64], drop_out_rate_2=-1., init_lr_2=0.002, train_batch_size=64)
+backend(n_layers_1=2, output_sizes_1=[16, 32], drop_out_rate_1=0.2, init_lr_1=0.01, n_layers_2=3, output_sizes_2=[32, 64, 64], drop_out_rate_2=-1., init_lr_2=0.002, epochs=10, train_batch_size=64, lr_step_gamma=0.7)
